50b8445b71c2985be963bce5a9c1b5892f0fc6fa
original-void <void321m@gmail.com>
2026-06-28 13:01:20 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,8 +2,8 @@ module.exports = {
     name: 'kick',
     async execute(sock, msg, { from, mentions, isGroup, botIsAdmin, senderIsAdmin }) {
         if(!isGroup) return sock.sendMessage(from, { text: '❌ Group only' });
-        if(!botIsAdmin) return sock.sendMessage(from, { text: `❌ Bot must be Admin 👑\nBot Admin = ${botIsAdmin}` });
-        if(!senderIsAdmin) return sock.sendMessage(from, { text: `❌ You must be Admin 👑\nYou Admin = ${senderIsAdmin}` });
+        if(!botIsAdmin) return sock.sendMessage(from, { text: `❌ Bot must be Admin 👑` });
+        if(!senderIsAdmin) return sock.sendMessage(from, { text: `❌ You must be Admin 👑` });
         
         const target = mentions[0];
         if (!target) return sock.sendMessage(from, { text: '👑 Usage: `.kick @user`' });
